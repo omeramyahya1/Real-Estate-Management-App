e2ab81bf6814e8fa28555effada7a246fc6d5ff2
omeramyahya <omeramyahya@protonmail.com>
2023-02-16 07:57:43 +0200

    1

--- App.js
+++ App.js
@@ -2,17 +2,65 @@ import "react-native-gesture-handler";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import welcome from "./screens/welcome";
 import dashboard from "./screens/dashboard";
 import signup from "./screens/signup";
 import login from "./screens/login";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Drawer.Navigator
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: "#c6cbef", //Set Drawer background
+            width: 250, //Set Drawer width
+          },
+          headerStyle: {
+            backgroundColor: "#f4511e", //Set Header color
+          },
+          headerTintColor: "#fff", //Set Header text color
+          headerTitleStyle: {
+            fontWeight: "bold", //Set Header text style
+          },
+        }}
+      >
+        <Drawer.Screen
+          name="dashboard"
+          options={{
+            drawerLabel: "First page Option",
+            title: "First Stack",
+          }}
+          component={<dashboard />}
+        />
+        <Drawer.Screen
+          name="login"
+          options={{
+            drawerLabel: "Second page Option",
+            title: "Second Stack",
+          }}
+          component={<login />}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+{
+  /* <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen
           options={{ gestureEnabled: false }}
           name="dashboard"
@@ -26,16 +74,5 @@ export default function App() {
 
         <Stack.Screen name="signup" component={signup} />
         <Stack.Screen name="login" component={login} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+      </Stack.Navigator> */
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
